d9725b02ba7b8f7edb51176f4f8a45196f62e606
Michal Luszczek <michluszcz@gmail.com>
2025-06-02 23:03:52 +0200

    working setup with proper structure

---
+++ sport-matcher/sport-matcher/src/main/java/paint/projekt/sport_matcher/SportMatcherApplication.java
@@ -0,0 +1,13 @@
+package paint.projekt.sport_matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportMatcherApplication.class, args);
+	}
+
+}
