055e4f32f87f6a78ecf723d113807c87478e0e2a
Michal Luszczek <michluszcz@gmail.com>
2025-06-02 23:58:06 +0200

    remove deprecated constructs

--- backend/sport-matcher/src/main/java/paint/projekt/sport_matcher/security/SecurityConfig.java
+++ backend/sport-matcher/src/main/java/paint/projekt/sport_matcher/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package paint.projekt.sport_matcher.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,12 +14,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable() // Disable CSRF for simplicity (not recommended for production)
+            .csrf(Customizer.withDefaults()) // Disable CSRF for simplicity (not recommended for production)
             .authorizeHttpRequests(authz -> authz
                 .anyRequest().permitAll() // Allow all requests without authentication
             )
-            .httpBasic().disable() // Disable basic auth
-            .formLogin().disable(); // Disable form login
+            .httpBasic(Customizer.withDefaults()) // Disable basic auth
+            .formLogin(Customizer.withDefaults()); // Disable form login
 
         return http.build();
     }
