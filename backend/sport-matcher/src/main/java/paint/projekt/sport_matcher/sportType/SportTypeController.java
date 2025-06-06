945166e0322f9e3f8a838a32d161385d678cde68
Michal Luszczek <michluszcz@gmail.com>
2025-06-06 19:59:02 +0200

    remove unused

--- backend/sport-matcher/src/main/java/paint/projekt/sport_matcher/sportType/SportTypeController.java
+++ backend/sport-matcher/src/main/java/paint/projekt/sport_matcher/sportType/SportTypeController.java
@@ -1,7 +1,6 @@
 package paint.projekt.sport_matcher.sportType;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- backend/sport-matcher/src/main/java/paint/projekt/sport_matcher/sportType/SportTypeRepository.java
+++ backend/sport-matcher/src/main/java/paint/projekt/sport_matcher/sportType/SportTypeRepository.java
@@ -3,7 +3,6 @@ package paint.projekt.sport_matcher.sportType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.nio.channels.FileChannel;
 import java.util.Optional;
 
 @Repository
