8c13061682485b4715f09afdbcfad76c64eee22e
KonradSL5 <01167290@pw.edu.pl>
2025-06-06 20:48:37 +0200

    Move slideshow to its own component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,11 @@
-import { useEffect, useRef, useState } from "react";
-// Images from freepik.com
-import basketball from "./assets/basketball.jpg";
-import football from "./assets/football.jpg";
-import tennis from "./assets/tennis.jpg";
 import "./App.css";
-
-const images = [basketball, football, tennis] as const;
+import ImageSlideshow from "./ImageSlideshow";
 
 function App() {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const slideTime = 5000;
-  const slideInterval = useRef(0);
-
-  useEffect(() => {
-    slideInterval.current = setInterval(() => {
-      setCurrentSlide((currentSlide + 1) % images.length);
-    }, slideTime);
-
-    return () => {
-      clearInterval(slideInterval.current);
-    };
-  });
   return (
     <main>
       <div className="stack fit">
-        {images.map((image, index) => {
-          const className = (() => {
-            if (index === currentSlide) return "image active";
-            else return "image";
-          })();
-          return <img id={`titleImage${index}`} key={index} className={className} src={image} />;
-        })}
+        <ImageSlideshow />
         <div className="textbox top">
           <h1>Sports Matcher</h1>
           <span>Bored? Find people to play sports games with!</span>
---
+++ frontend/src/ImageSlideshow.tsx
@@ -0,0 +1,35 @@
+import basketball from "./assets/basketball.jpg";
+import football from "./assets/football.jpg";
+import tennis from "./assets/tennis.jpg";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
+
+const images = [basketball, football, tennis] as const;
+
+export default function ImageSlideshow() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const slideTime = 5000;
+  const slideInterval = useRef(0);
+
+  useEffect(() => {
+    slideInterval.current = setInterval(() => {
+      setCurrentSlide((currentSlide + 1) % images.length);
+    }, slideTime);
+
+    return () => {
+      clearInterval(slideInterval.current);
+    };
+  });
+
+  return (
+    <>
+      {images.map((image, index) => {
+        const className = (() => {
+          if (index === currentSlide) return "image active";
+          else return "image";
+        })();
+        return <img id={`titleImage${index}`} key={index} className={className} src={image} />;
+      })}
+    </>
+  );
+}
