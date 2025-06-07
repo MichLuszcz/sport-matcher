ec652de2bb62e1a70e22c05279811d31cdb5f61a
Michal Luszczek <michluszcz@gmail.com>
2025-06-07 21:58:19 +0200

    enable security config to require token

--- backend/sport-matcher/src/main/java/paint/projekt/sport_matcher/security/SecurityConfig.java
+++ backend/sport-matcher/src/main/java/paint/projekt/sport_matcher/security/SecurityConfig.java
@@ -8,23 +8,43 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import paint.projekt.sport_matcher.config.AppConfiguration;
+
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter, UnauthorizedHandler unauthorizedHandler) throws Exception {
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         http
-            .csrf(AbstractHttpConfigurer::disable) // Disable CSRF for simplicity (not recommended for production)
-            .authorizeHttpRequests(authz -> authz
-                .anyRequest().permitAll() // Allow all requests without authentication
-            )
-            .httpBasic(Customizer.withDefaults()) // Disable basic auth
-            .formLogin(Customizer.withDefaults()); // Disable form login
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(h -> h.authenticationEntryPoint(unauthorizedHandler))
+                .securityMatcher("/**")
+                .authorizeHttpRequests(registry -> registry
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/users").permitAll()
+//                        .requestMatchers("/api/users/verify").permitAll()
+//                        .requestMatchers("/api/users/send-reset-email").permitAll()
+//                        .requestMatchers("/api/users/reset-password").permitAll()
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                );
 
         return http.build();
     }
@@ -42,6 +62,17 @@ public class SecurityConfig {
                 .passwordEncoder(passwordEncoder());
 
         return builder.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(AppConfiguration appConfiguration) {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(appConfiguration.getWebsiteBaseUrl()));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
 
