c8aba3f2ac640d0a925738e2c496aff085f74434
KonradSL5 <01167290@pw.edu.pl>
2025-06-08 23:36:22 +0200

    accessToken instead of token

--- frontend/src/components/AdForm.tsx
+++ frontend/src/components/AdForm.tsx
@@ -42,7 +42,7 @@ export default function AdForm() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${localStorage.getItem("token")}`
+          "Authorization": `Bearer ${localStorage.getItem("accessToken")}`
         },
         body: JSON.stringify(adData),
       });
